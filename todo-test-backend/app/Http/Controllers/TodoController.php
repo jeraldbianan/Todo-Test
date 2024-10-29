2af7802b7d7f80f25a6da9db52de1fb712e856f3
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-30 03:00:21 +0800

    added logic for store, update and destroy methods in TodoController

--- todo-test-backend/app/Http/Controllers/TodoController.php
+++ todo-test-backend/app/Http/Controllers/TodoController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTodoRequest;
+use App\Http\Requests\UpdateTodoRequest;
 use App\Http\Resources\TodoResource;
 use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+use function PHPSTORM_META\map;
 
 class TodoController extends Controller {
     /**
@@ -17,21 +22,39 @@ class TodoController extends Controller {
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) {
-        //
+    public function store(StoreTodoRequest $request) {
+        $request->validated();
+
+        $todo = Todo::create([
+            'title' => $request['title'],
+            'description' => $request['description'],
+            'status' => 'Pending',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        return new TodoResource($todo);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id) {
-        //
+    public function update(UpdateTodoRequest $request, Todo $todo) {
+        $request->validated();
+        $todo->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'status' => $request->status,
+            'updated_at' => Carbon::now()
+        ]);
+
+        return new TodoResource($todo);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id) {
-        //
+    public function destroy(Todo $todo) {
+        $todo->delete();
     }
 }
