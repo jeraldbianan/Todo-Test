a25292d26d4351f5b98145741019982ec71a6a03
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-30 00:23:10 +0800

    created a Todo model with migration, factory and seeder, and tested it to generate dummy data

---
+++ todo-test-backend/database/seeders/TodoSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Todo;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TodoSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        Todo::factory(10)->create();
+    }
+}
